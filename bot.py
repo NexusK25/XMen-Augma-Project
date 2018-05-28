37085756493ec3da5228a1e6048e954cf683219d
NexusK25 <39681147+NexusK25@users.noreply.github.com>
2018-05-28 12:12:30 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,64 @@
+# AugmaBot By NexusKirigaya [Nexus25#3905]
+
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import chalk
+
+
+startup_extensions = ["Music"]
+bot = commands.Bot(command_prefix='a!')
+
+@bot.event
+async def on_ready():
+    print ("Augma Activation In Progress...")
+    print ("Augma Project Activated!")
+    print ("With the ID: " + bot.user.id)
+
+class Main_Commands():
+    def __init__(self, bot):
+        self.bot = bot
+
+if __name__ == "__main__":
+    for extension in startup_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+                exc = '{}: {}'.format(type(e).__name__, e)
+                print('Failed to load extension {}\n{}'.format(extension, exc))
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+    await bot.say(":ping_pong: pong!")
+    print ("user has pinged")
+
+@bot.command(pass_context=True)
+async def info(ctx, user: discord.Member):
+    embed = discord.Embed(title="{}'s info".format(user.name), description="Ecco cosa ho potuto trovare.", color=0x7FFFD4)
+    embed.add_field(name="Name", value=user.name, inline=True)
+    embed.add_field(name="ID", value=user.id, inline=True)
+    embed.add_field(name="Satus", value=user.status, inline=True)
+    embed.add_field(name="Highest Role", value=user.top_role)
+    embed.add_field(name="Joined", value=user.joined_at)
+    embed.set_thumbnail(url=user.avatar_url)
+    await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+async def serverinfo(ctx):
+    embed = discord.Embed(name="{}'s info".format(ctx.message.server.name), description="Ecco cosa ho potuto trovare.", color=0xFFD800)
+    embed.set_author(name="Creato Da Nexus25 Di X-Men")
+    embed.add_field(name="Name", value=ctx.message.server.name, inline=True)
+    embed.add_field(name="ID", value=ctx.message.server.id, inline=True)
+    embed.add_field(name="Roles", value=len(ctx.message.server.roles), inline=True)
+    embed.add_field(name="Members", value=len(ctx.message.server.members))
+    embed.set_thumbnail(url=ctx.message.server.icon_url)
+    await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+@commands.has_role("[Founder]")
+async def kick(ctx, user: discord.Member):
+    await bot.say(":boot: Cya, {}. Ya loser!".format(user.name))
+    await bot.kick(user)
+
+bot.run ("NDQ4NTMyNTE2MzYyNjQ5NjEw.DesE5g.d3RAtZpCYJlSpqYgkMu7lR2OrE8")
